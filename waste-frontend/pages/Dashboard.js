b1d5fba3f5618c3168a553cef8facd87950dcc59
Deepak <deepak5859638@gmail.com>
2026-04-05 23:29:55 +0530

    pages deleted

---
+++ waste-frontend/pages/Dashboard.js
@@ -0,0 +1,12 @@
+﻿import React from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <p>This is a simple dashboard.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
